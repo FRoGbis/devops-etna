af7cc7acdcf49d903da8abb5694458d8894bf4d8
fx <etna@xavier-laptop>
2017-12-13 17:49:04 +0100

    add services and docker-compose

---
+++ docker/scripts/run_service.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y python3 python3-pip
+pip3 install -r /devops/services/$1/requirements.txt
+cd /devops/services/$1
+nameko run $1
--- docker/services/RegisterUser.py
+++
@@ -1,9 +0,0 @@
-from nameko.rpc import rpc
-
-class RegisterUser(object):
-	name = "registerUser"
-	
-	@rpc
-	def registerUser(self, user):
-		print ("User : " + user)
-		return "Test ok"
