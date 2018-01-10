b7361c080aeb75e51c2ba01e360778d40be07dee
fx <15982596+fauque@users.noreply.github.com>
2018-01-10 15:28:57 +0100

    suppression rabbit, refacto

--- docker/scripts/run_service.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-apt-get update
-apt-get install -y python3 python3-pip
-pip3 install -r /devops/services/$1/requirements.txt
-cd /devops/services/$1
-nameko run $1 --broker amqp://guest:guest@rabbitmq
